aa26c1c0674b36384639e4428d57a7118231d989
Albin Bergström <albinbergstrom@gmail.com>
2016-11-18 13:00:12 +0100

    Initial push?

--- Particle/ElementsOfAnimation/ElementsOfAnimation/controls.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROLS_HPP
-#define CONTROLS_HPP
-
-void computeMatricesFromInputs(const int window_w, const int window_h);
-glm::mat4 getViewMatrix();
-glm::mat4 getProjectionMatrix();
-
-#endif
