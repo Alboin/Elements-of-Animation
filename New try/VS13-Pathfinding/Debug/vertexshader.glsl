17ed6495294fc4806547f9c2abcc8e6b79da06d4
Albin Bergström <albinbergstrom@gmail.com>
2017-01-11 21:35:57 +0100

    final touches

---
+++ New try/VS13-Pathfinding/Debug/vertexshader.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+  
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+
+out vec3 vertexColor;
+out vec3 vertexNormal;
+
+uniform mat4 MVP;
+
+void main()
+{
+    gl_Position = MVP * vec4(position.x, position.y, position.z, 1.0);
+	vertexColor = color;
+}
